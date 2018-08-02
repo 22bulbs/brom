c4f8b4bed3115df7be1c4bf8fdd1ba89a0403e9d
s1gosain <sidhigosain@gmail.com>
2018-08-02 12:29:34 -0700

    reducers

--- src/client/reducers/index.js
+++ src/client/reducers/index.js
@@ -5,7 +5,10 @@ const reducersFile = require('./transactionsReducer');
 const reducers = combineReducers({
   transactions: reducersFile.transactionsReducer,
   transactionMethodFilter: reducersFile.transactionMethodFilterReducer,
-  selectedTransactionIndex: reducersFile.selectedTransactionIndexReducer
+  selectedTransactionIndex: reducersFile.selectedTransactionIndexReducer,
+  transactionApiFilter: reducersFile.transactionApiFilterReducer,
+  transactionFlagFilter: reducersFile.transactionFlagFilterReducer,
+  transactionDomainFilter: reducersFile.transactionDomainFilterReducer
 })
 
 export default reducers;
