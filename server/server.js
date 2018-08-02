b7ce1700775d2c8b8ef2c1cfebe4ceea7482a418
Joe Thel <jgraythel@gmail.com>
2018-08-02 14:52:14 -0700

    ✨ ♻  clean up route-reader to just do its job
    - Configure results server to write to a file if environment variable
    exists
    - Remove unnecessary actions from route-reader
    - Remove redundant user server (now that port is a required argument)

--- server/server.js
+++ server/server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const fs = require('fs');
 const bodyParser = require('body-parser');
 
 
@@ -10,31 +11,43 @@ const io = require('socket.io')(server);
 
 const transactions = [];
 
-console.log('hey i\'m a server');
 app.use(express.static('tempclient'));
 app.use(express.static('dist'));
 app.use(bodyParser.json());
 
-app.get('/test',(req, res) => {
-	io.emit('transaction', {key:'value'});
-	res.end();
-})
+app.get('/test', (req, res) => {
+  io.emit('transaction', { key: 'value' });
+
+  const destination = process.env.WEBHEAD_WRITE_DESTINATION;
+  if (destination) {
+    fs.writeFile(destination, JSON.stringify(transactions, null, 2), (err) => {
+      if (err) {
+        console.error(`Failed to write results to ${destination}`);
+        console.error(err);
+        return;
+      }
+      console.log(`Wrote results to ${destination}`);
+    });
+  }
+
+  res.end();
+});
 
 app.post('/results', (req, res) => {
-	console.log('hit results route');
-	if (req.body instanceof Array) {
-		transactions.push(...req.body);
-	} else {
-		transactions.push(req.body);
-	}
-	io.emit('transaction', req.body);
-	io.emit('test', 'hello');
-	res.end();
-})
-io.on('connection', function(socket){
+  console.log('hit results route');
+  if (req.body instanceof Array) {
+    transactions.push(...req.body);
+  } else {
+    transactions.push(req.body);
+  }
+  io.emit('transaction', req.body);
+  io.emit('test', 'hello');
+  res.end();
+});
+io.on('connection', (socket) => {
   console.log('a user connected');
   socket.emit('transaction', transactions);
-  socket.on('disconnect', function(){
+  socket.on('disconnect', () => {
     console.log('user disconnected');
   });
 });
