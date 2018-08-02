6c498878ca13e9e67c467446ffa5c1a90946daf1
Ryan Trontz <rtrontz@gmail.com>
2018-08-01 20:09:19 -0700

    basic container and component setup

---
+++ src/client/components/DetailsTransactionSummary.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const DetailsTransactionSummary = props => (
+  <div>DetailsTransactionSummary</div>
+);
+
+export default DetailsTransactionSummary;
+
+
---
+++ src/client/components/TransactionList.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+const TransactionList = props => (
+  <div>TransactionList</div>
+);
+
+export default TransactionList;
+
