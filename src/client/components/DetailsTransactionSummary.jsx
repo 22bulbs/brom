5adf824b940f2f2304303aad8d58a06a54bf32f8
s1gosain <sidhigosain@gmail.com>
2018-08-01 22:28:06 -0700

    updated rendering

--- src/client/components/DetailsTransactionSummary.jsx
+++ src/client/components/DetailsTransactionSummary.jsx
@@ -1,7 +1,20 @@
 import React from 'react';
 
 const DetailsTransactionSummary = props => (
-  <div>DetailsTransactionSummary</div>
+  <div>DetailsTransactionSummary
+    <div className="method">
+    method example
+    </div>
+    <div className="route">
+    route example
+    </div>
+    <div className="icon">
+    domain request
+    </div>
+    <div className="icon">
+    icon
+    </div>
+  </div>
 );
 
 export default DetailsTransactionSummary;
