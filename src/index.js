9fa5c73b45bf936aaecc7d6e2f0402a86639493e
Ryan Trontz <rtrontz@gmail.com>
2018-08-08 14:44:38 -0700

    adding a dummy dispatch to trigger on app load, but after store is intiialized

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from "react-dom";
 import { Provider } from 'react-redux';
 import App from "./App.js";
 import store from './client/store';
+import { addTransaction } from "./client/actions/actions.js";
+import { sampleData } from "../sampleData.js";
 
 console.log('hello!');
 ReactDOM.render(
@@ -11,3 +13,5 @@ ReactDOM.render(
   </Provider>,
     document.getElementById("root")
 );
+
+store.dispatch(addTransaction(sampleData));
