9b3a0a3e3c02aa9082cf76ee9470807f3e980da7
s1gosain <sidhigosain@gmail.com>
2018-08-09 20:52:52 -0700

    warning works finally

--- src/client/containers/TransactionDetailsContainer.jsx
+++ src/client/containers/TransactionDetailsContainer.jsx
@@ -3,27 +3,37 @@ import { connect } from 'react-redux';
 import DetailsTransactionSummary from '../components/DetailsTransactionSummary.jsx';
 import DetailsRequestContainer from './DetailsRequestContainer.jsx';
 import DetailsResponseContainer from './DetailsResponseContainer.jsx';
-
+import { bindActionCreators } from '../../../../../../Library/Caches/typescript/2.9/node_modules/redux';
+import * as actions from '../actions/actions'
 
 const mapStateToProps = state => ({
-  
+  transactions: state.transactions,
+  selectedTransactionIndex: state.selectedTransactionIndex
 });
 
 const mapDispatchToProps = dispatch => ({
-  
+ 
 });
 
-export default class TransactionDetailsContainer extends Component {
+class TransactionDetailsContainer extends Component {
   constructor(props) {
     super(props);
   }
 
   render(){
+    const transactions = this.props.transactions[this.props.selectedTransactionIndex];
+    if (!transactions) return <div>State empty</div>
     return(
       <div id='transaction-details-container' className="flex-row">
+        <div id='warning'>
+        Warning
+        {JSON.stringify(transactions.warnings.res)}
+        </div>
         <DetailsRequestContainer />
         <DetailsResponseContainer />
       </div>  
     )
   }
 }
+
+export default connect(mapStateToProps, mapDispatchToProps)(TransactionDetailsContainer)
