328e0f6af4e6586714291b82f166df5355c2db7d
Joe Thel <jgraythel@gmail.com>
2018-08-13 15:39:45 -0700

    💄 style TransactionList components

--- src/client/components/TransactionItem.jsx
+++ src/client/components/TransactionItem.jsx
@@ -3,23 +3,26 @@ import React from 'react';
 
 const methodStyle = {
   borderRight: '1px solid black',
-  width: '75px'
+  width: '75px',
+  textAlign: 'center',
 }
 
 const TransactionItem = ({ id, onTransactionClick, method, url, isExternal, flags, isSelected }) => {
   const itemStyle = {
-    border: '1px solid black',
-    color: (isSelected) ? 'red' : 'black', 
+    borderBottom: '1px solid black',
+    color: (isSelected) ? 'red' : 'black',
   }
 
   return (
     <div className="flex-row transaction-item" style={itemStyle} id={id} onClick={() => onTransactionClick(id)}>
       <div className="transaction-item-method" style={methodStyle} >
-        <h4>{method}</h4>
+        <h3>{method}</h3>
       </div>
       <div className="transaction-item-endpoint">
         <p>{url}</p>
-        {isExternal} <span>{flags}</span>
+        <div className="transaction-item-icons">
+          <span>{isExternal}</span> <span>{flags}</span>
+        </div>
       </div>
     </div>
   );
