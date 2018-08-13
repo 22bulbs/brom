6f5a22501c1c8d654d9e002d6ac0f9cec96f4360
Joe Thel <jgraythel@gmail.com>
2018-08-13 13:59:54 -0700

    ♻ change globalData port keys to single ports object

--- src/index.js
+++ src/index.js
@@ -11,37 +11,39 @@ ReactDOM.render(
   <Provider store={store}>
     <App />
   </Provider>,
-    document.getElementById("root")
+  document.getElementById("root")
 );
 
-// uncomment the following line when not testing the back end 
+// uncomment the following line when not testing the back end
 store.dispatch(addTransaction(sampleData));
 store.dispatch(updateGlobalData({
-    'application-port': 3000,
-    'proxy-port': 9999,
-    'results-port': 7193,
-    title: 'demo title',
-    protocol: 'http',
-    methods: {
-      GET: 55,
-      POST: 23,
-      DELETE: 10,
-      PUT: 33,
-      PATCH: 17,
-      JOKE: 13,
-      HAHA: 1,
-      LITTLEMORE: 4
-    },
-    totals: {
-      transactions: 59,
-      severe: 12,
-      deprecated: 3,
-      conflicting: 1,
-      redundant: 0,
-      internal: 29,
-      external: 30
-    }
-  }));
+  ports: {
+    app: 3000,
+    proxy: 9999,
+    results: 7913,
+  },
+  title: 'demo title',
+  protocol: 'http',
+  methods: {
+    GET: 55,
+    POST: 23,
+    DELETE: 10,
+    PUT: 33,
+    PATCH: 17,
+    JOKE: 13,
+    HAHA: 1,
+    LITTLEMORE: 4
+  },
+  totals: {
+    transactions: 59,
+    severe: 12,
+    deprecated: 3,
+    conflicting: 1,
+    redundant: 0,
+    internal: 29,
+    external: 30
+  }
+}));
 
 // uncomment the following block for use with backend and web sockets
 // const socket = io();
