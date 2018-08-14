68172dd6fd82aa4da5f8e4f49beec52ecc734069
nathanielgrossman <nategrossman@mac.com>
2018-08-14 13:11:04 -0700

    added icons to right side, added victory animation

--- src/client/components/DestinationDonut.jsx
+++ src/client/components/DestinationDonut.jsx
@@ -6,6 +6,7 @@ const DestinationDonut = ({internal, external}) => (
     <svg viewBox="0 0 400 400" width="160" height="160">
       <VictoryPie
         standalone={false}
+        animate={{duration: 500}}
         renderInPortal={false}
         width={400} height={400}
         data={[
