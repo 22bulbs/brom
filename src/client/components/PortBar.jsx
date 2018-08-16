10332fe3a9f520c9ee5538d9f5a613666d0e3a2c
nathanielgrossman <nategrossman@mac.com>
2018-08-15 22:44:55 -0700

    port bar layout

--- src/client/components/PortBar.jsx
+++ src/client/components/PortBar.jsx
@@ -5,13 +5,17 @@ import Icon from './Icon';
 const PortBar = ({ app, proxy, results }) => (
   <div id="port-bar" className="border-bottom light-bg">
     <span id="brom-title">brom</span>
-    <div>
+    <div className='ports'>
       <span>
         <LabeledText label="Application Port" text={app} />|
         <LabeledText label="Proxy Port" text={proxy} />|
         <LabeledText label="Results Port" text={results} />
       </span>
-      <Icon flag="connected" />
+      <span style={{
+        color: 'green'
+      }}>
+        <Icon flag="connected" />
+      </span>
     </div>
   </div>
 );
